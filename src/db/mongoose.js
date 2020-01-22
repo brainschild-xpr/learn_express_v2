c4519fc1bcd281930222c0d9a9d4e693c2c921ec
XPR <shungoh.martin@gmail.com>
2020-01-23 01:57:36 +0300

    update use of .env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,17 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/auth',{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}).then(() =>{
-  console.log('connected to database');
-}).catch(() =>{
+const dbAuth = process.env.DB_auth
+const mongoUri = process.env.DB_Uri
+const db_auth_url = mongoUri + dbAuth
+
+mongoose.connect(db_auth_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to database_name:', dbAuth);
+}).catch(() => {
   console.log('failed connected to database');
 });
